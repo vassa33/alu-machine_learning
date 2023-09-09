b8a0ff4cb722f1f11e341d3898a2bf7d64f8265d
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-09 18:42:16 +0300

    PEP 8

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -18,11 +18,13 @@ def add_matrices2D(mat1, mat2):
     None: If mat1 and mat2 are not the same shape.
     """
     # Check if mat1 and mat2 have the same shape
-    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+    if len(mat1) != len(mat2) or any(len(row1) != len(row2) 
+                                     for row1, row2 in zip(mat1, mat2)):
         return None
 
     # Perform element-wise addition
-    result = [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    result = [[a + b for a, b in zip(row1, row2)] 
+              for row1, row2 in zip(mat1, mat2)]
     return result
 
 
