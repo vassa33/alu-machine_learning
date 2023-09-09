75ac9acb5d02e38f492eca469f25f7c3bf112f01
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-09 17:49:00 +0300

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-matrix_shape = __import__('2-size_me_please').matrix_shape
-
 def matrix_shape(matrix):
     """
     Calculate the shape (dimensions) of a matrix.
@@ -17,3 +15,11 @@ def matrix_shape(matrix):
         shape.append(len(matrix))
         matrix = matrix[0]
     return shape
+
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    print(matrix_shape(mat1))
+    
+    mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+            [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+    print(matrix_shape(mat2))
