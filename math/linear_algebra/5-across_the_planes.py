a5f8de4e36f35703e023cdbd38aeb01fbc111435
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-09 18:52:43 +0300

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,6 +4,7 @@
 This module defines a function for adding 2D matrices element-wise
 """
 
+
 def add_matrices2D(mat1, mat2):
     """
     Add two 2D matrices element-wise.
@@ -17,14 +18,15 @@ def add_matrices2D(mat1, mat2):
     None: If mat1 and mat2 are not the same shape.
     """
     # Check if mat1 and mat2 have the same shape
-    if len(mat1) != len(mat2) or any(len(row1) != len(row2) 
+    if len(mat1) != len(mat2) or any(len(row1) != len(row2)
                                      for row1, row2 in zip(mat1, mat2)):
         return None
 
     # Perform element-wise addition
-    result = [[a + b for a, b in zip(row1, row2)] 
+    result = [[a + b for a, b in zip(row1, row2)]
               for row1, row2 in zip(mat1, mat2)]
     return result
+
 
 if __name__ == "__main__":
     add_matrices2D = __import__('5-across_the_planes').add_matrices2D
