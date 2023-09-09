34412c880bcf9868dd01b112fb89389c6ec2a29a
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-09 22:18:49 +0300

    Update 102-squashed_like_sardines.py

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -4,7 +4,7 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     """
-    Concatenate two matrices along a specific axis.
+    Concatenates two matrices along a specific axis.
 
     Args:
     mat1 (list): The first matrix.
@@ -12,8 +12,8 @@ def cat_matrices(mat1, mat2, axis=0):
     axis (int): The axis along which to concatenate. Default is 0.
 
     Returns:
-    list: A new matrix resulting from concatenation along the specified axis.
-    None: If matrices cannot be concatenated due to shape mismatch.
+    list: A new matrix resulting from concatenation.
+    None: If concatenation is not possible.
     """
 
     def shape_of_matrix(matrix):
@@ -26,41 +26,38 @@ def cat_matrices(mat1, mat2, axis=0):
     shape1 = shape_of_matrix(mat1)
     shape2 = shape_of_matrix(mat2)
 
-    if axis >= len(shape1) or axis >= len(shape2):
+    # Check if the shapes of both matrices match along the specified axis
+    if shape1[:axis] != shape2[:axis] or shape1[axis + 1:] != shape2[axis + 1:]:
         return None
 
-    # Check if dimensions along the specified axis match
-    if shape1[axis] != shape2[axis]:
-        return None
+    def concatenate_matrices(matrix1, matrix2):
+        # Recursively concatenate matrices along the specified axis
+        if isinstance(matrix1, list):
+            return [concatenate_matrices(m1, m2) for m1, m2 in zip(matrix1, matrix2)]
+        else:
+            return matrix1 + matrix2
 
-    # Concatenate matrices along the specified axis
     if axis == 0:
-        return mat1 + mat2
+        return concatenate_matrices(mat1, mat2)
+    elif axis == 1:
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
-        return [cat_matrices(mat1[i], mat2[i], axis - 1) for i in range(len(mat1))]
+        return None
+
 
 # Test cases
 if __name__ == "__main__":
-    mat1 = [1, 2, 3]
-    mat2 = [4, 5, 6]
+    mat1 = [[11, 22, 33], [44, 55, 66]]
+    mat2 = [[1, 2, 3], [4, 5, 6]]
     print(cat_matrices(mat1, mat2))
-
-    mat1 = [[1, 2], [3, 4]]
-    mat2 = [[5, 6], [7, 8]]
-    print(cat_matrices(mat1, mat2))
+    print(cat_matrices(mat1, mat2, axis=1))
+    print(cat_matrices(mat1, mat2, axis=2))
 
     mat3 = [[[[1, 2, 3, 4], [5, 6, 7, 8]],
-             [[9, 10, 11, 12], [13, 14 ,15, 16]],
+             [[9, 10, 11, 12], [13, 14, 15, 16]],
              [[17, 18, 19, 20], [21, 22, 23, 24]]]]
     mat4 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
-             [[19, 110, 111, 112], [113, 114 ,115, 116]],
-             [[117, 118, 119, 120], [121, 122, 123, 124]]]]
-    mat5 = [[[[11, 12, 13, 14], [15, 16, 17, 18]],
+             [[19, 110, 111, 112], [113, 114, 115, 116]],
              [[117, 118, 119, 120], [121, 122, 123, 124]]]]
     
     print(cat_matrices(mat3, mat4, axis=3))
-    print(cat_matrices(mat3, mat5, axis=1))
-
-    # Invalid concatenation due to shape mismatch
-    mat6 = [9, 10]
-    print(cat_matrices(mat1, mat6))
