7509c43b651867c23de54dee61a8e347c310f9d0
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-09 20:20:48 +0300

    Update 11-the_western_exchange.py

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,30 +1,7 @@
 #!/usr/bin/env python3
-
-"""
-This module defines a function for transposing a numpy.ndarray
-"""
+"""The Western Exchange"""
 
 
 def np_transpose(matrix):
-    """
-    Transpose a numpy.ndarray without using loops, conditional statements, or imports.
-
-    Args:
-    matrix (numpy.ndarray): The input matrix to be transposed.
-
-    Returns:
-    numpy.ndarray: A new transposed numpy.ndarray.
-    """
-    # Determine the shape of the input matrix
-    shape = matrix.shape
-
-    # Create a new matrix with transposed shape
-    transposed = np.empty(shape[::-1], dtype=matrix.dtype)
-
-    # Calculate the index mapping for transposition
-    index_map = np.arange(shape[0])[:, np.newaxis], np.arange(shape[1])
-
-    # Rearrange the elements based on the index mapping
-    transposed[index_map] = matrix
-
-    return transposed
+    """transposes matrix"""
+    return matrix.T
