df3bef3a7b9f62ccc098bdcb73041e73c1d4e050
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-09 19:02:59 +0300

    PEP 8

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -14,7 +14,7 @@ def cat_arrays(arr1, arr2):
     arr2 (list): The second array.
 
     Returns:
-    list: A new list containing the elements of arr1 
+    list: A new list containing the elements of arr1
     followed by the elements of arr2.
     """
     return arr1 + arr2
