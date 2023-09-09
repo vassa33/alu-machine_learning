afe990946adccb1ad27b010bc25b7bc9c1640f7c
vassa33 <peggyongoma@gmail.com>
2023-09-09 17:44:30 +0300

    Module documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """
+    Calculate the shape (dimensions) of a matrix.
+
+    Args:
+    matrix (list): A nested list representing the matrix.
+
+    Returns:
+    list: A list of integers representing the dimensions of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
