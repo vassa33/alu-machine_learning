290208922f31109bbbd1433ea4d050e18f61d309
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-09-30 11:25:47 +0300

    Poisson PMF, CDF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -2,17 +2,48 @@
 """A class Poisson representing a Poisson Distribution"""
 
 
-class Poisson:
+class Poisson():
     """Poisson Distribution"""
 
     def __init__(self, data=None, lambtha=1.):
         if data is None:
             if lambtha <= 0:
-                raise ValueError('lambtha must be a positive value')
+                raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
-                raise TypeError('data must be a list')
-            if len(data) < 2:
-                raise ValueError('data must contain multiple values')
-            self.lambtha = float(sum(data) / len(data))
+                raise TypeError("data must be a list")
+            if len(data) <= 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data)) / len(data)
+
+    def pmf(self, k):
+        """calculates the value of the PMF for a given number of “successes”"""
+        
+        if k < 0:
+            return 0
+        if type(k) is not int:
+            k = int(k)
+
+        # calculates
+        def factorial(n):
+            if n == 0:
+                return 1
+            else:
+                return n * factorial(n - 1)
+
+        e = 2.7182818285
+        return ((self.lambtha ** k) * (1 / (e ** self.lambtha))) / factorial(k)
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”"""
+        
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        else:
+            res = 0
+            for i in range(k + 1):
+                res += self.pmf(i)
+            return res
