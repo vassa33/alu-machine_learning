e152c319132fc0d11f92b22f2d04a2d775c05420
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-10-08 15:45:18 +0300

    Grayscale convolution

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""a function that performs a valid convolution on grayscale images"""
+
+import numpy as np
+
+def convolve_grayscale_valid(images, kernel):
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    output_h = h - kh + 1
+    output_w = w - kw + 1
+    output = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            output[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+
+    return output
