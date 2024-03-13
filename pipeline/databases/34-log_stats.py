178bba546ffd8cbe2f2ed0de7060587c66700137
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2024-03-13 19:57:09 +0300

    Update 34-log_stats.py

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
-""" Task 34 """
+"""
+Provides some stats about Nginx logs stored in MongoDB
+"""
+
+
 from pymongo import MongoClient
 
-template = """\
-{} logs
-Methods:
-\tmethod GET: {}
-\tmethod POST: {}
-\tmethod PUT: {}
-\tmethod PATCH: {}
-\tmethod DELETE: {}
-{} status check"""
 
 if __name__ == "__main__":
+    """
+    Gets stats about Nginx logs stored in MongoDB
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client['logs']
-    collection = db['nginx']
-
-    logs = collection.count_documents({})
-    GET = collection.count_documents({"method": "GET"})
-    POST = collection.count_documents({"method": "POST"})
-    PUT = collection.count_documents({"method": "PUT"})
-    PATCH = collection.count_documents({"method": "PATCH"})
-    DELETE = collection.count_documents({"method": "DELETE"})
-    status = collection.count_documents({"method": "GET",
-                                         "path": "/status"})
-
-    print(template.format(logs, GET, POST, PUT, PATCH, DELETE, status))
+    logs_coll = client.logs.nginx
+    doc_count = logs_coll.count_documents({})
+    print("{} logs".format(doc_count))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = logs_coll.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    filter_path = {"method": "GET", "path": "/status"}
+    path_count = logs_coll.count_documents(filter_path)
+    print("{} status check".format(path_count))
