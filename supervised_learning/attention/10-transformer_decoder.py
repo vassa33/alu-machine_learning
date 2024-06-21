57efe06360b0590e74549ef94ef02a6052d3a8d7
Vanessa Ongoma <61325877+vassa33@users.noreply.github.com>
2024-06-21 22:53:26 +0300

    fix pycodestyle

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -53,7 +53,7 @@ class Decoder(tf.keras.layers.Layer):
         x = self.dropout(x, training=training)
 
         for i in range(self.N):
-            x = self.blocks[i](x, encoder_output,
+            x = self.blocks[i](x, encoder_output, 
                                training,look_ahead_mask, padding_mask)
 
         return x
