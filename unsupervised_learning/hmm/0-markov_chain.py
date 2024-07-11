6d5595a3658dc8fb25b922f6375628860136485b
vassa33 <p.otieno@alustudent.com>
2024-07-11 13:09:14 +0000

    fix pycodestyle

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def markov_chain(P, s, t=1):
     """
-        Determines the probability of a markov chain being in a particular state
+        Determines probability of a markov chain being in a particular state
         after a specified number of iterations
         :param P: square 2D numpy.ndarray of shape (n, n) representing the
         transition matrix
@@ -16,7 +16,7 @@ def markov_chain(P, s, t=1):
             n is the number of states in the markov chain
         :param s: numpy.ndarray of shape (1, n) representing the probability of
         starting in each state
-        :param t: the number of iterations that the markov chain has been through
+        :param t: number of iterations that the markov chain has been through
         :return: numpy.ndarray of shape (1, n) representing the probability of
         being in a specific state after t iterations, or None on failure
     """
