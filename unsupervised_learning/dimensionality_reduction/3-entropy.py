f3128943273fdfe1b93ce8413fc971d0bcee4961
vassa33 <p.otieno@alustudent.com>
2024-07-11 11:57:33 +0000

    fix pycodestyle

--- unsupervised_learning/dimensionality_reduction/3-entropy.py
+++ unsupervised_learning/dimensionality_reduction/3-entropy.py
@@ -7,15 +7,15 @@ import numpy as np
 
 def HP(Di, beta):
     """
-        Calculates the Shannon entropy and P affinities relative to a data point
+        Calculates Shannon entropy & P affinities relative to a data point
         :param Di: numpy.ndarray of shape (n - 1,) containing the pariwise
         distances between a data point and all other points except itself
             n is the number of data points
         :param beta: the beta value for the Gaussian distribution
         :return: (Hi, Pi)
             Hi: the Shannon entropy of the points
-            Pi: a numpy.ndarray of shape (n - 1,) containing the P affinities of
-            the points
+            Pi: a numpy.ndarray of shape (n - 1,) containing the P
+            affinities of the points
     """
     num = (np.exp(- Di.copy() * beta))
     den = (np.sum(np.exp(-Di.copy() * beta)))
