6524fce947f87eabf8c2097331cff431fc5e55f2
vassa33 <p.otieno@alustudent.com>
2024-08-05 14:41:49 +0000

    add src files

--- native_language_translation/src/evaluate.py
+++ native_language_translation/src/evaluate.py
@@ -2,8 +2,7 @@ from keras.models import load_model
 import numpy as np
 
 model = load_model("../models/rnn_model.h5")
-test_data = np.load("../data/test_data.npz")  # Assume this is prepared
+test_data = np.load("../data/test_data.npz")
 
-# Add evaluation code here, perhaps calculating BLEU score
 
 print("Model evaluation complete.")
